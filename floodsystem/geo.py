4cd96398ff1db83026764007a8ab489d1a50d3c1
jepohle <jep78@cam.ac.uk>
2022-01-22 19:14:35 +0000

    Added description of function

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,10 +6,13 @@ geographical data.
 
 """
 import numpy as np
-from utils import sorted_by_key  # noqa
+from floodsystem.utils import sorted_by_key  # noqa
 
 
 def stations_by_distance(stations, p):
+    """Takes a list of measuring stations and calculates the distance to a given coordinate p.
+    Then sorts this list and outputs a touple of format (station, distance)."""
+
     list_by_distance = []
     for station in stations:
         distance = np.sqrt((station.coord[0] - p[0])**2 + (station.coord[1] - p[1])**2)
