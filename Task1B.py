d01689476e53fee5b7977aa1fea0bb03bd4dc3bd
jepohle <jep78@cam.ac.uk>
2022-01-22 19:58:14 +0000

    Added printout for the furthest stations: completed demostrator code.

--- Task1B.py
+++ Task1B.py
@@ -10,9 +10,18 @@ def run():
         my_station = station_and_distance[0]
         tuple = (my_station.name, my_station.town, station_and_distance[1])
         list.append(tuple)
-    print(list)
-
     
+    list_far = []
+    for n in range(10):
+        station_and_distance = i[n-10]
+        my_station = station_and_distance[0]
+        tuple = (my_station.name, my_station.town, station_and_distance[1])
+        list_far.append(tuple)
+    print("The ten closest measuring stations are:", list)
+    print()
+    print("The ten furthest measuring stations are:", list_far)
+
+
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
     run()
