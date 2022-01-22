b364a89c4b1b08eb4ab59b5a636ea371c989f0d7
jepohle <jep78@cam.ac.uk>
2022-01-22 19:51:48 +0000

    Task 1B demonstration code completed

--- Task1B.py
+++ Task1B.py
@@ -3,10 +3,14 @@ from floodsystem.stationdata import build_station_list
 
 def run():
     stations = build_station_list()
-    
-    i = stations_by_distance(stations, (0,0))
-    print(i[:10])
-
+    i = stations_by_distance(stations, (52.2053,0.1218))
+    list = []
+    for n in range(10):
+        station_and_distance = i[n]
+        my_station = station_and_distance[0]
+        tuple = (my_station.name, my_station.town, station_and_distance[1])
+        list.append(tuple)
+    print(list)
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
     run()
