e142b8e972ed7a8e052ae9123ef4b1a402abedc4
Kirill Kaliteevskiy <kk753@cam.ac.uk>
2022-01-22 17:48:24 +0000

    task 1B added to geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,8 +5,13 @@
 geographical data.
 
 """
+import numpy as np
+from utils import sorted_by_key  # noqa
 
-from .utils import sorted_by_key  # noqa
 
 def stations_by_distance(stations, p):
-    
+    list_by_distance = []
+    for station in stations:
+        distance = np.sqrt((station.coord[0] - p[0])**2 + (station.coord[1] - p[1])**2)
+        list_by_distance.append((station, distance))
+    return sorted_by_key(list_by_distance,1)
