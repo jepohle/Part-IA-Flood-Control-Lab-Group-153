4ad4ae382ab60c2d0c337c0bdc8144d006999fff
jepohle <jep78@cam.ac.uk>
2022-01-26 14:17:46 +0000

    Function for updating and fetching river level data implemented into code

--- Task2B.py
+++ Task2B.py
@@ -1,8 +1,9 @@
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 
 def run():
     x = build_station_list()
+    update_water_levels(x)
     stations = stations_level_over_threshold(x, 0.8)
     for station in stations:
         stationob = station[0]
