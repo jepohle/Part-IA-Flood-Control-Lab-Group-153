3b2166f94b3baba0fa005e9585d72f079e0fdacc
jepohle <jep78@cam.ac.uk>
2022-01-26 15:35:35 +0000

    Added description and bugfix

--- test_flood.py
+++ test_flood.py
@@ -2,6 +2,7 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.flood import stations_highest_rel_level, stations_level_over_threshold
 
 def test_stations_level_over_threshold():
+    """Test for the function returning a sorted list of tuples (station, relative water level) of stations with relative water level over a threshold."""
     x = build_station_list()
     station = stations_level_over_threshold(x, 0.5)
     levels = []
@@ -13,11 +14,12 @@ def test_stations_level_over_threshold():
         assert levels[i+1] <= levels[i]
 
 def test_stations_highest_rel_level():
+    """Test for the function that returns the N stations with the highest relative water level."""
     x = build_station_list()
     station = stations_highest_rel_level(x, 10)
     levels = []
     for i in station:
         levels.append(i.relative_water_level())
-    assert len(levels) == 10
+    assert len(station) == 10
     for i in range(len(levels)):
         assert levels[i+1] <= levels[i]
