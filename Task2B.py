a2aa9ee965508e343347701d252daf15cffd001d
jepohle <jep78@cam.ac.uk>
2022-01-26 13:42:56 +0000

    Initial commit of script as required per Task 2B

---
+++ Task2B.py
@@ -0,0 +1,14 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_level_over_threshold
+
+def run():
+    x = build_station_list()
+    stations = stations_level_over_threshold(x, 0.8)
+    for station in stations:
+        stationob = station[0]
+        level = station[1]
+        print(stationob.name, level)
+
+if __name__ == "__main__":
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    run()
